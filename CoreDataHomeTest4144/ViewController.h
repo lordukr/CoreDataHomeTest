8b642758b6a24d111a4ed76a42b053ee3186b558
Anatoli Zavialov <mymac@macbook-pro.local>
2016-05-30 13:05:33 +0300

    Initial Commit

---
+++ CoreDataHomeTest4144/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataHomeTest4144
+//
+//  Created by My mac on 30.05.16.
+//  Copyright © 2016 Anatolii Zavialov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
